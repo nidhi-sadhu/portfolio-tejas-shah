3b43a7aeaf8f12174d2cdd3dc3483f2060947c31
nidhi.sadhu2097 <nidhi.sadhu2097@gmail.com>
2025-06-07 23:40:04 -0500

    installed primeng icons properly

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { CvComponent } from './cv/cv.component';
-import { SkillsComponent } from './skills/skills.component';
-import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // Single page with all sections
